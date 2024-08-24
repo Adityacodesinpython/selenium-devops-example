9bd4411b82b566e62ef7e419d1ba520e76bd16f0
Aditya <141542441+Adityacodesinpython@users.noreply.github.com>
2024-08-24 22:04:40 +0530

    Update ChromeDriverTest.java

--- src/test/java/org/movoto/selenium/example/ChromeDriverTest.java
+++ src/test/java/org/movoto/selenium/example/ChromeDriverTest.java
@@ -1,28 +1,18 @@
-package org.movoto.selenium.example;
-
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.MarionetteDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by haozuo on 3/22/16.
- */
 public class ChromeDriverTest {
 
     private String testUrl;
@@ -30,39 +20,34 @@ public class ChromeDriverTest {
 
     @Before
     public void prepare() {
-        //setup chromedriver
+        // Set up ChromeDriver
         System.setProperty(
                 "webdriver.chrome.driver",
-                "webdriver/chromedriver");
+                "C:/path/to/chromedriver.exe");  // Use the absolute path
 
         testUrl = "https://www.youtube.com/";
 
-        // Create a new instance of the Chrome driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
+        // Initialize ChromeDriver
         driver = new ChromeDriver();
 
-        //maximize window
+        // Maximize window
         driver.manage().window().maximize();
 
-        // And now use this to visit myBlog
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to(testUrl);
+        // Navigate to test URL
         driver.get(testUrl);
     }
 
     @Test
     public void testTitle() throws IOException {
-
         // Find elements by attribute lang="READ_MORE_BTN"
         List<WebElement> elements = driver
                 .findElements(By.cssSelector("[lang=\"READ_MORE_BTN\"]"));
 
-        //Click the selected button
+        // Click the selected button
         elements.get(0).click();
 
-
-        assertTrue("The page title should be chagned as expected",
+        // Wait for the page title to change and assert it
+        assertTrue("The page title should be changed as expected",
                 (new WebDriverWait(driver, 5))
                         .until(new ExpectedCondition<Boolean>() {
                             public Boolean apply(WebDriver d) {
@@ -70,12 +55,13 @@ public class ChromeDriverTest {
                             }
                         })
         );
-
     }
 
     @After
     public void teardown() throws IOException {
-        driver.quit();
+        // Check if driver is not null before quitting
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
